b1483cf1a5cd6e1c127d255b9fa6183460b1e997
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-10-02 23:27:14 -0400

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        firstColor: '#222831',
+        secondColor: '#00fff5',
+        thirdColor: '#c5c6c7'
+      }
+    },
+  },
+  plugins: [],
+}
+
+/*66fcf1 bright blue */
+/*45a29e dull blue */
