94415d070b79e4d80b02c5dd346291b70cebbf2d
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-10-04 18:05:32 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Blogs from './components/Blogs';
+import Features from './components/Features';
+import Main from './components/Main';
+
 
 function App(){
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
     return (
-        <div className=''>
-            
-        </div>
+        <BrowserRouter>
+            <div className=''>
+                <Navbar isLoggedIn={isLoggedIn}/>
+                <Routes>
+                    <Route path='/' element={Main} />
+                    <Route path='/features' element={Features} />
+                    <Route path='/signIn' element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
+                    {isLoggedIn && <Route path='/blogs' element={Blogs} />}
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
 }
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                Logo - BlogVault
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
