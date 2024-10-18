db7b3cbe7284c562d7955632df09db8fae6c739b
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-10-18 00:16:45 -0400

    correction

---
+++ backend/db/db.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const db_url = process.env.MONGO_URL;
+
+mongoose.connect(db_url);
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String
+});
+
+const BlogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    content: String
+});
+
+const Users = mongoose.model('UserSchema');
+const Blogs = mongoose.model('BlogSchema');
+
+
+/*const connection = mongoose.connect(db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Connection established!');
+}).catch((err)=> {
+    console.log('Connection error!', err);
+    process.exit(1);
+})*/
+
+module.exports = {
+    Users,
+    Blogs
+}
--- backend/models/Blogs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BlogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    content: String
-})
-
-module.exports = mongoose.model('Blogs', BlogSchema)
