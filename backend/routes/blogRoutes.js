ec650c0c7e815c14702684c0780bd5098d7dcacf
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-10-18 03:05:01 -0400

    routes

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,19 +1,54 @@
 const express = require('express');
+const Blogs = require('../middlewares/blogs');
+const AuthMiddleware = require('../middlewares/auth');
 
-/*const app = express()
-app.use(express.json())*/
+const router = express.Router();
 
-const router = express.Router()
+router.post('/newBlog', AuthMiddleware, async (req,res) => {
+    const {title, content} = req.body;
+    const author = req.user.username;
+    const newBlog = new Blogs({title, author, content});
 
-router.post('/newBlog', (req, res) => {
-
+    try{
+        await newBlog.save();
+        res.status(201).json({
+            msg: "Blog Created!",
+            blog: newBlog
+        })
+    } catch(err) {
+        res.status(500).json({
+            msg: "Error creating blog!"
+        });
+    }
 })
 
-router.get('/existingBlog', (req, res) => {
-    title: req.params.title;
-    author: req.params.author;
-    content: req.params.content
+router.get('/allBlogs', async (req, res) => {
+    try{
+        const blogs = await Blogs.find();
+        res.status(200).json(blogs)
+    } catch(err){
+        res.status(500).json({
+            msg: "Unable to fetch blogs"
+        })
+    }
 })
 
+router.get('/existingBlogs/:title', async(req, res){
+    const title = req.params.title;
+    try {
+        const blogs = await Blogs.findOne({ title });
+        if (blog){
+            res.status(200).json(blog)
+        } else{
+            res.status(404).json({
+                msg: "Blog not found!"
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            msg: "Error fetching blog!"
+        })
+    }
+})
 
 module.exports = router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const Users = require('../db/db');
+const jwt = require('jsonwebtoken');
+const UserMiddleware = require('../middlewares/users');
+
+const router = express.Router()
+
+router.post('/login', UserMiddleware, (req, res) => {
+    const username = req.headers.username;
+    const token = jwt.sign({username: username}, process.env.JWT_SECRET, {expiresIn: '1h'});
+    res.status(200).json({
+        msg: "Logged in successfully!",
+        token
+    });
+})
+
+model.exports = router;
